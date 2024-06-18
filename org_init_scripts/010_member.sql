36d9a02316937420665e051d99dfaa393fb03178
leefordjudes <leefordjudes@gmail.com>
2024-06-18 15:31:58 +0530

    u

--- org_init_scripts/010_member.sql
+++ org_init_scripts/010_member.sql
@@ -1,7 +1,7 @@
 create table if not exists member_role
 (
     id         int       not null generated always as identity primary key,
-    name       text      not null unique,
+    name       text      not null,
     perms      text[],
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp,
