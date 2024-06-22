a9d63010d91904913df9d33aeeab2cb15f117438
esakkimuthu-p <esakkimuthu556@gmail.com>
2024-06-22 13:15:20 +0530

    stock location name as primary key

--- org_init_scripts/016_stock_location.sql
+++ org_init_scripts/016_stock_location.sql
@@ -1,7 +1,6 @@
 create table if not exists stock_location
 (
-    id         int       not null generated always as identity primary key,
-    name       text      not null,
+    name       text      not null primary key,
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp,
     constraint name_min_length check (char_length(trim(name)) > 0)
