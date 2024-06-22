5fe1606da03048869aa98b346d1a6e97b26d70c5
leefordjudes <leefordjudes@gmail.com>
2024-06-22 10:08:06 +0530

    revstore acc data

--- org_data/006_pre_account.sql
+++ org_data/006_pre_account.sql
@@ -1,14 +1,15 @@
 CREATE TABLE TEMP_ACC (
   ID TEXT NOT NULL PRIMARY KEY,
   NAME TEXT,
-  ACCOUNT_TYPE_ID INT,
-  CONTACT_TYPE TEXT,
+  CONTACT_TYPE TEXT,  
+  -- ACCOUNT_TYPE_ID INT,
+  ACCOUNT_TYPE_NAME TEXT,
   GST_REG_TYPE TEXT,
   GST_LOCATION_ID TEXT,
   GST_NO TEXT,
   PAN_NO TEXT,
-  BILL_WISE_DETAIL BOOL,
-  DUE_BASED_ON TEXT,
+  -- BILL_WISE_DETAIL BOOL,
+  -- DUE_BASED_ON TEXT,
   MOBILE TEXT,
   TELEPHONE TEXT,
   EMAIL TEXT,
@@ -27,22 +28,36 @@ as
 $$
 declare
     cur_task text := '';
+    ac_type_id int;
+    billwisedetail bool;
+    duebasedon typ_due_based_on;
 begin
     begin
-    cur_task = format('check_gst_no - %s: %s',new.gst_no, check_gst_no(new.gst_no));
-    if check_gst_no(new.gst_no) then 
-        cur_task = format('gstno true, insert account id: %s', new.id);
-        INSERT INTO account(NAME, ACCOUNT_TYPE_ID, CONTACT_TYPE, GST_REG_TYPE, GST_LOCATION_ID, GST_NO, PAN_NO, BILL_WISE_DETAIL, 
-        DUE_BASED_ON, MOBILE, TELEPHONE, EMAIL, CONTACT_PERSON, ADDRESS, CITY, PINCODE, STATE_ID, COUNTRY_ID) VALUES 
-        (new.name, new.account_type_id, new.contact_type::typ_contact_type, new.gst_reg_type::typ_gst_reg_type, new.gst_location_id, new.gst_no, new.pan_no, new.bill_wise_detail,
-        new.due_based_on::typ_due_based_on, new.mobile, new.telephone, new.email, new.contact_person, new.address, new.city, new.pincode, new.state_id, new.country_id);
-        delete from temp_acc where id=new.id;
-    else 
-        cur_task = format('gstno false, insert account id: %s', new.id);
-        INSERT INTO account(NAME, ACCOUNT_TYPE_ID, CONTACT_TYPE, GST_REG_TYPE, GST_LOCATION_ID, PAN_NO, BILL_WISE_DETAIL, 
-        DUE_BASED_ON, MOBILE, TELEPHONE, EMAIL, CONTACT_PERSON, ADDRESS, CITY, PINCODE, STATE_ID, COUNTRY_ID) VALUES 
-        (new.name, new.account_type_id, new.contact_type::typ_contact_type, new.gst_reg_type::typ_gst_reg_type, new.gst_location_id, new.pan_no, new.bill_wise_detail,
-        new.due_based_on::typ_due_based_on, new.mobile, new.telephone, new.email, new.contact_person, new.address, new.city, new.pincode, new.state_id, new.country_id);
+    cur_task = format('getting account_type_id for account_type_name: %s',new.account_type_name);
+    select id into ac_type_id from account_type where name=new.account_type_name or default_name::text=new.account_type_name;
+    if new.contact_type in ('VENDOR','CUSTOMER','AGENT') then
+      billwisedetail=true;
+      duebasedon='EFF_DATE'::typ_due_based_on;
+    end if;
+    cur_task = format('account type name: %s id: %s',new.account_type_name, ac_type_id);
+    if ac_type_id is not null then
+    cur_task = format('ac_type_id: %s, check_gst_no - %s: %s',ac_type_id, new.gst_no, check_gst_no(new.gst_no));
+      if new.gst_no is not null AND check_gst_no(new.gst_no) then
+          cur_task = format('gstno true, insert account id: %s', new.id);
+          INSERT INTO account(NAME, ACCOUNT_TYPE_ID, CONTACT_TYPE, GST_REG_TYPE, GST_LOCATION_ID, GST_NO, PAN_NO, 
+          BILL_WISE_DETAIL, DUE_BASED_ON, MOBILE, TELEPHONE, EMAIL, CONTACT_PERSON, ADDRESS, CITY, PINCODE, STATE_ID, COUNTRY_ID) VALUES 
+          (new.name, ac_type_id, new.contact_type::typ_contact_type, new.gst_reg_type::typ_gst_reg_type, new.gst_location_id, new.gst_no, new.pan_no,
+          billwisedetail, duebasedon, new.mobile, new.telephone, new.email, new.contact_person, new.address, new.city, new.pincode, new.state_id, new.country_id);
+          delete from temp_acc where id=new.id;
+      else 
+          cur_task = format('gstno false, insert account id: %s', new.id);
+          INSERT INTO account(NAME, ACCOUNT_TYPE_ID, CONTACT_TYPE, GST_REG_TYPE, GST_LOCATION_ID, PAN_NO, 
+          BILL_WISE_DETAIL, DUE_BASED_ON, MOBILE, TELEPHONE, EMAIL, CONTACT_PERSON, ADDRESS, CITY, PINCODE, STATE_ID, COUNTRY_ID) VALUES 
+          (new.name, ac_type_id, new.contact_type::typ_contact_type, new.gst_reg_type::typ_gst_reg_type, new.gst_location_id, new.pan_no, 
+          billwisedetail, duebasedon, new.mobile, new.telephone, new.email, new.contact_person, new.address, new.city, new.pincode, new.state_id, new.country_id);
+      end if;
+    else
+        raise exception '%',cur_task;
     end if;
     exception when others then
       raise exception '%',cur_task;
