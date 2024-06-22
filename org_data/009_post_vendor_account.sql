96f6912dd9d6e8669b9820634a28e3b429e3b10f
leefordjudes <leefordjudes@gmail.com>
2024-06-22 12:41:17 +0530

    org data removed

--- org_data/009_post_vendor_account.sql
+++
@@ -1,5 +0,0 @@
-drop trigger if exists trig_account_from_temp_acc on temp_acc;
---##
-drop function if exists fn_account_from_temp_acc;
---##
---drop table if exists TEMP_ACC;
--- src/organization.rs
+++ src/organization.rs
@@ -1,13 +1,7 @@
-use std::fs;
-
-use axum::{
-    extract::{Path, State},
-    http::StatusCode,
-};
-use sea_orm::{ConnectionTrait, DbBackend, Statement};
-use tenant::init::{init_organization, Organization};
+use axum::{extract::State, http::StatusCode};
 
 use crate::AppState;
+use tenant::init::{init_organization, Organization};
 
 pub async fn organization_init(
     State(state): State<AppState>,
@@ -24,86 +18,4 @@ pub async fn organization_init(
         "msg": "Organization init successful.",
     });
     Ok(res.into())
-}
-
-pub async fn restore_data(
-    State(state): State<AppState>,
-    Path((name,)): Path<(String,)>,
-) -> Result<axum::Json<bool>, (StatusCode, String)> {
-    let db = &state.db.get(&name).await;
-    let mut files: Vec<String> = Vec::new();
-    let mut file_order: Vec<u16> = Vec::new();
-    let mut file_paths: Vec<String> = Vec::new();
-    println!("Restore Data For Organization {name}");
-    match fs::read_dir("./org_data/") {
-        Ok(dirs) => {
-            for dir in dirs.flatten() {
-                let path = dir.path().to_string_lossy().to_string();
-                file_paths.push(path.clone());
-                let order = (path.replace("./org_data/", "")[0..3].to_string())
-                    .parse::<u16>()
-                    .map_err(|_| {
-                        (
-                            StatusCode::INTERNAL_SERVER_ERROR,
-                            "Couldnot get file order".to_owned(),
-                        )
-                    })?;
-
-                file_order.push(order);
-            }
-        }
-        Err(_) => {
-            return Err((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "could not read backup data folder".into(),
-            ));
-        }
-    }
-    file_order.sort();
-    for ord in file_order {
-        let filepath = file_paths
-            .iter()
-            .find_map(|x| x.contains(&format!("/{:03}_", ord)).then_some(x.clone()))
-            .unwrap_or_default()
-            .to_string();
-        if !filepath.is_empty() {
-            match fs::read_to_string(&filepath) {
-                Ok(file) => {
-                    files.push(file);
-                }
-                Err(_) => {
-                    return Err((
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("could not read file {}", &filepath),
-                    ));
-                }
-            }
-        }
-    }
-
-    println!("\nSecuring db started.\n");
-    for f in files {
-        let stmts = f.split("--##").collect::<Vec<&str>>();
-        println!("statements: {:?}", &stmts);
-        for stmt in stmts {
-            println!("\nRunning:\n{}\n", &stmt);
-            let _ = db
-                .execute(Statement {
-                    sql: stmt.to_string(),
-                    values: None,
-                    db_backend: DbBackend::Postgres,
-                })
-                .await
-                .map_err(|_| {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("Couldnot run script: {}", &stmt),
-                    )
-                })?;
-            println!("\nCompleted\n");
-        }
-    }
-    println!("\ndata restore completed.\n");
-
-    Ok(true.into())
 }
