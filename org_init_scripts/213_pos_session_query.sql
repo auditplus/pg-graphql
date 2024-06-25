4dbf9e46252289afd265651a1fa1920b3614b432
esakkimuthu-p <esakkimuthu556@gmail.com>
2024-06-25 10:57:29 +0530

    upd

--- org_init_scripts/213_pos_session_query.sql
+++ org_init_scripts/213_pos_session_query.sql
@@ -1,13 +1,20 @@
-create function pos_current_session_transacted_accounts(counter_id int) returns jsonb as
+create function pos_current_session_transacted_accounts(counter_id int)
+    returns table
+            (
+                account_id         int,
+                account_name       text,
+                base_account_types text[]
+            )
+as
 $$
 begin
-    return (with a as (select distinct (account_id) as acc_id
-                       from pos_counter_transaction_breakup
-                       where session_id is null
-                         and pos_counter_id = $1)
-            select jsonb_agg(jsonb_build_object('account_id', a.acc_id, 'account_name', b.name))
-            from a
-                     left join account b on b.id = a.acc_id);
+    return query (with a as (select distinct (pt.account_id) as acc_id
+                             from pos_counter_transaction_breakup pt
+                             where session_id is null
+                               and pos_counter_id = $1)
+                  select b.id, b.name, b.base_account_types
+                  from a
+                           left join account b on b.id = a.acc_id);
 end;
 $$ language plpgsql immutable
                     security definer;
