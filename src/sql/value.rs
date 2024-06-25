812cdb9a7e8b0e147207d38942ed31dc4181ebae
esakkimuthu-p <esakkimuthu556@gmail.com>
2024-06-25 10:41:54 +0530

    upd

--- src/sql/value.rs
+++ src/sql/value.rs
@@ -1,22 +1,25 @@
+use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum SQLArrayType {
     Bool,
-    TinyInt,
+    Float,
     SmallInt,
     Int,
     BigInt,
+    String,
 }
 
 impl From<SQLArrayType> for sea_orm::sea_query::ArrayType {
     fn from(value: SQLArrayType) -> Self {
         match value {
             SQLArrayType::Bool => sea_orm::sea_query::ArrayType::Bool,
-            SQLArrayType::TinyInt => sea_orm::sea_query::ArrayType::TinyInt,
+            SQLArrayType::Float => sea_orm::sea_query::ArrayType::Float,
             SQLArrayType::SmallInt => sea_orm::sea_query::ArrayType::SmallInt,
             SQLArrayType::Int => sea_orm::sea_query::ArrayType::Int,
             SQLArrayType::BigInt => sea_orm::sea_query::ArrayType::BigInt,
+            SQLArrayType::String => sea_orm::sea_query::ArrayType::String,
         }
     }
 }
@@ -24,7 +27,7 @@ impl From<SQLArrayType> for sea_orm::sea_query::ArrayType {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "t", content = "v")]
 pub enum SQLValue {
-    //Bool(Option<bool>),
+    Bool(Option<bool>),
     //TinyInt(Option<i8>),
     //SmallInt(Option<i16>),
     Int(Option<i32>),
@@ -33,13 +36,13 @@ pub enum SQLValue {
     //SmallUnsigned(Option<u16>),
     //Unsigned(Option<u32>),
     //BigUnsigned(Option<u64>),
-    //Float(Option<f32>),
+    Float(Option<f32>),
     Double(Option<f64>),
     String(Option<Box<String>>),
     //Char(Option<char>),
     //Bytes(Option<Box<Vec<u8>>>),
     Json(Option<Box<sea_orm::query::JsonValue>>),
-    //ChronoDate(Option<Box<NaiveDate>>),
+    ChronoDate(Option<Box<NaiveDate>>),
     //ChronoTime(Option<Box<NaiveTime>>),
     //ChronoDateTime(Option<Box<NaiveDateTime>>),
     //ChronoDateTimeUtc(Option<Box<DateTime<Utc>>>),
@@ -58,11 +61,14 @@ pub enum SQLValue {
 impl From<SQLValue> for sea_orm::Value {
     fn from(value: SQLValue) -> Self {
         match value {
+            SQLValue::Bool(v) => sea_orm::Value::Bool(v),
+            SQLValue::Float(v) => sea_orm::Value::Float(v),
             SQLValue::Double(v) => sea_orm::Value::Double(v),
             SQLValue::String(v) => sea_orm::Value::String(v),
             SQLValue::Json(v) => sea_orm::Value::Json(v),
             SQLValue::Int(v) => sea_orm::Value::Int(v),
             SQLValue::Uuid(v) => sea_orm::Value::Uuid(v),
+            SQLValue::ChronoDate(v) => sea_orm::Value::ChronoDate(v),
             SQLValue::Array(t, v) => {
                 let o = v.map(|x| Box::new(x.into_iter().map(|y| y.into()).collect()));
                 sea_orm::Value::Array(t.into(), o)
