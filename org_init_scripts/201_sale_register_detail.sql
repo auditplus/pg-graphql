1ad967f80c4079a0dbef6dc7a03b4c1ea9eb0ea0
esakkimuthu-p <esakkimuthu556@gmail.com>
2024-07-01 17:58:16 +0530

    upd

--- org_init_scripts/201_sale_register_detail.sql
+++ org_init_scripts/201_sale_register_detail.sql
@@ -56,18 +56,17 @@ create function sale_register_group(input_data json)
 as
 $$
 declare
-    branches      int[]                 := (select array_agg(j::int)
-                                            from json_array_elements_text(($1 ->> 'branches')::json) as j);
-    customers     int[]                 := (select array_agg(j::int)
-                                            from json_array_elements_text(($1 ->> 'customers')::json) as j);
-    payment_modes text[]                := (select array_agg(j::text)
-                                            from json_array_elements_text(($1 ->> 'payment_modes')::json) as j);
-    view          typ_base_voucher_type := ($1 ->> 'view')::typ_base_voucher_type;
+    branches      int[]  := (select array_agg(j::int)
+                             from json_array_elements_text(($1 ->> 'branches')::json) as j);
+    customers     int[]  := (select array_agg(j::int)
+                             from json_array_elements_text(($1 ->> 'customers')::json) as j);
+    payment_modes text[] := (select array_agg(j::text)
+                             from json_array_elements_text(($1 ->> 'payment_modes')::json) as j);
 begin
     if upper($1 ->> 'group_by') not in ('MONTH', 'DAY') then
         raise exception 'invalid group_by value';
     end if;
-    if view not in ('SALE', 'CREDIT_NOTE') then
+    if ($1 ->> 'view')::text not in ('SALE', 'CREDIT_NOTE', 'BOTH') then
         raise exception 'invalid view';
     end if;
     if ($1 ->> 'group_by_branch')::bool then
@@ -83,7 +82,9 @@ begin
                    sum(a.gift_voucher_amount)
             from sale_register_detail a
             where date between ($1 ->> 'from_date')::date and ($1 ->> 'to_date')::date
-              and case when view is not null then a.base_voucher_type = view else true end
+              and case
+                      when ($1 ->> 'view')::text = 'BOTH' then true
+                      else a.base_voucher_type = ($1 ->> 'view')::typ_base_voucher_type end
               and case when array_length(branches, 1) > 0 then a.branch_id = any (branches) else true end
               and case when array_length(customers, 1) > 0 then a.customer_id = any (customers) else true end
               and case when 'cash' = any (payment_modes) then a.cash_amount <> 0 else true end
@@ -106,7 +107,9 @@ begin
                    sum(a.gift_voucher_amount)
             from sale_register_detail a
             where date between ($1 ->> 'from_date')::date and ($1 ->> 'to_date')::date
-              and case when view is not null then a.base_voucher_type = view else true end
+              and case
+                      when ($1 ->> 'view')::text = 'BOTH' then true
+                      else a.base_voucher_type = ($1 ->> 'view')::typ_base_voucher_type end
               and case when array_length(branches, 1) > 0 then a.branch_id = any (branches) else true end
               and case when array_length(customers, 1) > 0 then a.customer_id = any (customers) else true end
               and case when 'cash' = any (payment_modes) then a.cash_amount <> 0 else true end
@@ -133,15 +136,14 @@ create function sale_register_summary(input_data json)
 as
 $$
 declare
-    branches      int[]                 := (select array_agg(j::int)
-                                            from json_array_elements_text(($1 ->> 'branches')::json) as j);
-    customers     int[]                 := (select array_agg(j::int)
-                                            from json_array_elements_text(($1 ->> 'customers')::json) as j);
-    payment_modes text[]                := (select array_agg(j::text)
-                                            from json_array_elements_text(($1 ->> 'payment_modes')::json) as j);
-    view          typ_base_voucher_type := ($1 ->> 'view')::typ_base_voucher_type;
+    branches      int[]  := (select array_agg(j::int)
+                             from json_array_elements_text(($1 ->> 'branches')::json) as j);
+    customers     int[]  := (select array_agg(j::int)
+                             from json_array_elements_text(($1 ->> 'customers')::json) as j);
+    payment_modes text[] := (select array_agg(j::text)
+                             from json_array_elements_text(($1 ->> 'payment_modes')::json) as j);
 begin
-    if view not in ('SALE', 'CREDIT_NOTE') then
+    if ($1 ->> 'view')::text not in ('SALE', 'CREDIT_NOTE', 'BOTH') then
         raise exception 'invalid view';
     end if;
     return query
@@ -153,7 +155,9 @@ begin
                sum(a.gift_voucher_amount)
         from sale_register_detail a
         where date between ($1 ->> 'from_date')::date and ($1 ->> 'to_date')::date
-          and case when view is not null then a.base_voucher_type = view else true end
+          and case
+                  when ($1 ->> 'view')::text = 'BOTH' then true
+                  else a.base_voucher_type = ($1 ->> 'view')::typ_base_voucher_type end
           and case when array_length(branches, 1) > 0 then a.branch_id = any (branches) else true end
           and case when array_length(customers, 1) > 0 then a.customer_id = any (customers) else true end
           and case when 'cash' = any (payment_modes) then a.cash_amount <> 0 else true end
