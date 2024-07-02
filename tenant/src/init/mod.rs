f3cdcdd8d464adb5d9131f71cd35ad6624a74d68
hchockarprasad <hchockarprasad@gmail.com>
2024-07-02 09:46:28 +0530

    t

--- tenant/src/init/mod.rs
+++ tenant/src/init/mod.rs
@@ -2,11 +2,51 @@ mod script;
 
 use anyhow::Result;
 use regex::Regex;
-use sea_orm::{
-    ConnectionTrait, Database, DatabaseBackend, DatabaseConnection, DbBackend, Statement,
-};
+use sqlx_postgres::{PgPool, PgTypeInfo, Postgres};
+
 use serde::{Deserialize, Serialize};
+use sqlx::database::HasValueRef;
+use sqlx::error::BoxDynError;
+use sqlx::{Column, Decode, Row, Type};
 use std::path::Path;
+
+#[derive(sqlx::Type, Debug)]
+#[sqlx(type_name = "color")]
+#[sqlx(rename_all = "UPPERCASE")]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+#[derive(sqlx::Type, Debug)]
+#[sqlx(type_name = "sub_address")]
+struct SubAddress {
+    door: String,
+}
+
+#[derive(Debug)]
+struct Address {
+    street: String,
+    sub: SubAddress,
+}
+
+impl<'r> Decode<'r, Postgres> for Address {
+    fn decode(
+        value: <Postgres as HasValueRef<'r>>::ValueRef,
+    ) -> std::result::Result<Self, BoxDynError> {
+        let mut decoder = sqlx::postgres::types::PgRecordDecoder::new(value)?;
+        let street = decoder.try_decode::<String>()?;
+        let sub = decoder.try_decode::<SubAddress>()?;
+        Ok(Address { street, sub })
+    }
+}
+
+impl Type<Postgres> for Address {
+    fn type_info() -> <Postgres as sqlx::Database>::TypeInfo {
+        PgTypeInfo::with_name("address")
+    }
+}
 
 use script::Scripts;
 
@@ -30,7 +70,7 @@ pub async fn init_organization<P>(
     conn_uri: &str,
     organization: Organization,
     init_script_path: P,
-) -> Result<DatabaseConnection>
+) -> Result<()>
 where
     P: AsRef<Path>,
 {
@@ -41,45 +81,65 @@ where
         .ok_or(anyhow::Error::msg("Invalid organization name"))?;
 
     // create database
-    let conn = Database::connect(conn_uri).await?;
-    let stmt = Statement::from_sql_and_values(
-        DatabaseBackend::Postgres,
-        "select datname from pg_database where datname = $1 LIMIT 1",
-        [organization.name.clone().into()],
-    );
-    let db = if conn.query_one(stmt).await?.is_some() {
-        // connect to existing database
-        let db_url = format!("{}/{}", &conn_uri, &organization.name);
-        Database::connect(db_url).await?
-    } else {
-        // create database
-        conn.execute_unprepared(&format!("CREATE DATABASE {}", &organization.name))
-            .await?;
-        conn.close().await?;
-        println!("Database created");
+    let pool = PgPool::connect(conn_uri).await?;
+    let row = sqlx::query("select * from my_table LIMIT 1")
+        .bind(organization.name)
+        .fetch_one(&pool)
+        .await?;
+    let mut map = serde_json::Map::new();
 
-        // connect to created database
-        let db_url = format!("{}/{}", &conn_uri, &organization.name);
-        let db = Database::connect(db_url).await?;
-
-        let scripts = Scripts::from_dir(&init_script_path)?;
-        for script in scripts {
-            for stmt in script {
-                db.execute_unprepared(&stmt).await?;
-            }
+    for (idx, col) in row.columns().iter().enumerate() {
+        if let Ok(v) = row.try_get::<String, usize>(idx) {
+            println!("Text: {:?}", &v);
+            continue;
         }
+        if let Ok(v) = row.try_get::<i32, usize>(idx) {
+            println!("{:?}", &v);
+            continue;
+        }
+        //if let Ok(v) = row.try_get::<Color, usize>(idx) {
+        //    println!("{:?}", &v);
+        //    continue;
+        //}
+        if let Ok(v) = row.try_get::<Address, usize>(idx) {
+            println!("{:?}", &v);
+            continue;
+        }
+    }
 
-        // Execute create organization function
-        let org_input_data = serde_json::to_value(organization).unwrap();
-        let stm = Statement::from_sql_and_values(
-            DbBackend::Postgres,
-            "select * from create_organization($1)",
-            [org_input_data.into()],
-        );
-        db.execute(stm).await?;
-        db
-    };
-    Ok(db)
+    //let db = if conn.query_one(stmt).await?.is_some() {
+    //    // connect to existing database
+    //    let db_url = format!("{}/{}", &conn_uri, &organization.name);
+    //    Database::connect(db_url).await?
+    //} else {
+    //    // create database
+    //    conn.execute_unprepared(&format!("CREATE DATABASE {}", &organization.name))
+    //        .await?;
+    //    conn.close().await?;
+    //    println!("Database created");
+
+    //    // connect to created database
+    //    let db_url = format!("{}/{}", &conn_uri, &organization.name);
+    //    let db = Database::connect(db_url).await?;
+
+    //    let scripts = Scripts::from_dir(&init_script_path)?;
+    //    for script in scripts {
+    //        for stmt in script {
+    //            db.execute_unprepared(&stmt).await?;
+    //        }
+    //    }
+
+    //    // Execute create organization function
+    //    let org_input_data = serde_json::to_value(organization).unwrap();
+    //    let stm = Statement::from_sql_and_values(
+    //        DbBackend::Postgres,
+    //        "select * from create_organization($1)",
+    //        [org_input_data.into()],
+    //    );
+    //    db.execute(stm).await?;
+    //    db
+    //};
+    Ok(())
 }
 
 #[tokio::test]
@@ -100,5 +160,5 @@ async fn test_init() {
         "../org_init_scripts/",
     )
     .await;
-    assert!(db.is_ok());
+    //assert!(db.is_ok());
 }
