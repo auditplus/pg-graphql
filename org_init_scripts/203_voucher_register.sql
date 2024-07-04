8d9e8a404cc649da60db7ddb999a8101e97dd3de
esakkimuthu-p <esakkimuthu556@gmail.com>
2024-07-04 15:56:22 +0530

    report nodified

--- org_init_scripts/203_voucher_register.sql
+++ org_init_scripts/203_voucher_register.sql
@@ -19,30 +19,36 @@ from voucher;
 comment on view voucher_register_detail is e'@graphql({"primary_key_columns": ["id"]})';
 --##
 create function voucher_register_summary(input json)
-    returns setof json as
+    returns table
+            (
+                particulars   date,
+                voucher_count bigint
+            )
+as
 $$
 declare
-    br_ids     int[]                   := (select array_agg(j::int)
+    branches   int[]                   := (select array_agg(j::int)
                                            from json_array_elements_text((input ->> 'branches')::json) as j);
     base_types typ_base_voucher_type[] := (select array_agg(j::typ_base_voucher_type)
-                                           from json_array_elements_text((input ->> 'baseVoucherTypes')::json) as j);
+                                           from json_array_elements_text((input ->> 'base_voucher_types')::json) as j);
 begin
+    if upper($1 ->> 'group_by') not in ('MONTH', 'DAY') then
+        raise exception 'invalid group_by value';
+    end if;
     return query
-        with s1 as (select date_trunc((input ->> 'group')::text, date)::date as particulars,
-                           count(1)                                         as c
-                    from voucher_register_detail
-                    where (date between (input ->> 'fromDate')::date and (input ->> 'toDate')::date)
-                      and (case when array_length(br_ids, 1) > 0 then branch_id = ANY (br_ids) else true end)
-                      and (case
-                               when array_length(base_types, 1) > 0 then base_voucher_type = ANY (base_types)
-                               else true end)
-                      and (case
-                               when input ->> 'mode' is not null then mode = (input ->> 'mode')::typ_voucher_mode
-                               else true end)
-                    group by particulars
-                    order by particulars)
-        select json_build_object('particular', particulars, 'voucherCount', c)
-        from s1;
+        select date_trunc((input ->> 'group_by')::text, date)::date as particulars,
+               count(1)
+        from voucher_register_detail
+        where (date between (input ->> 'from_date')::date and (input ->> 'to_date')::date)
+          and (case when array_length(branches, 1) > 0 then branch_id = ANY (branches) else true end)
+          and (case
+                   when array_length(base_types, 1) > 0 then base_voucher_type = ANY (base_types)
+                   else true end)
+          and (case
+                   when input ->> 'mode' is not null then mode = (input ->> 'mode')::typ_voucher_mode
+                   else true end)
+        group by particulars
+        order by particulars;
 end;
 $$ language plpgsql immutable
                     security definer;
