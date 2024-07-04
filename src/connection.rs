b264a91e431f3ed58679ab9a4fccea8be544d264
Antony Judes <leefordjudes@gmail.com>
2024-07-04 18:12:16 +0530

    stream data (#67)

--- src/connection.rs
+++ src/connection.rs
@@ -1,5 +1,3 @@
-use crate::cdc;
-use channel::Receiver;
 use sea_orm::DatabaseConnection;
 use std::collections::HashMap;
 use std::sync::Arc;
@@ -8,32 +6,22 @@ use tokio::sync::RwLock;
 #[derive(Clone)]
 pub struct DbConnection {
     pools: Arc<RwLock<HashMap<String, DatabaseConnection>>>,
-    cdc_receiver: Arc<RwLock<HashMap<String, Receiver<cdc::Transaction>>>>,
 }
 
 impl Default for DbConnection {
     fn default() -> Self {
         DbConnection {
             pools: Arc::new(RwLock::new(HashMap::new())),
-            cdc_receiver: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 }
 
 impl DbConnection {
     pub async fn add(&self, name: &str, pool: DatabaseConnection) {
-        let (tx, rx) = channel::bounded::<cdc::Transaction>(100);
-        let org_name = name.to_string();
         self.pools.write().await.insert(name.to_string(), pool);
-        tokio::spawn(async move { cdc::watch(org_name, tx).await });
-        self.cdc_receiver.write().await.insert(name.to_string(), rx);
     }
     pub async fn get(&self, db_name: &str) -> DatabaseConnection {
         self.pools.read().await.get(db_name).unwrap().clone()
-    }
-
-    pub async fn cdc_rx(&self, db_name: &str) -> Receiver<cdc::Transaction> {
-        self.cdc_receiver.read().await.get(db_name).unwrap().clone()
     }
 
     pub async fn list(&self) -> Vec<String> {
--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -4,11 +4,12 @@ use crate::rpc::constants::*;
 use crate::session::Session;
 use crate::{cdc, AppState};
 use axum::body::Bytes;
-use axum::extract::ws::WebSocket;
+use axum::extract::ws::{Message, WebSocket};
 use axum::extract::{State, WebSocketUpgrade};
 use axum::response::IntoResponse;
 use channel::Receiver;
 use once_cell::sync::Lazy;
+use serde::Serialize;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
@@ -22,14 +23,45 @@ mod connection;
 
 mod constants;
 
+#[derive(Debug, Serialize)]
+pub struct ListenChannelResponse<T>
+where
+    T: Serialize + std::fmt::Debug,
+{
+    channel: &'static str,
+    data: T,
+}
+
+pub async fn start_db_change_stream(rx: Receiver<cdc::Transaction>) {
+    while let Ok(txn) = rx.recv().await {
+        let data = ListenChannelResponse {
+            channel: "db_changes",
+            data: txn,
+        };
+        let data = serde_json::to_string(&data).unwrap();
+
+        for s in WEBSOCKETS.read().await.iter() {
+            if s.1
+                .read()
+                .await
+                .channels
+                .0
+                .send(Message::Text(data.clone()))
+                .await
+                .is_err()
+            {
+                println!("Error on sending db changes");
+            }
+        }
+    }
+}
+
 pub async fn get_handler(
     State(app_state): State<AppState>,
     session: Session,
     ws: WebSocketUpgrade,
 ) -> impl IntoResponse {
     let id = Uuid::new_v4();
-
-    let cdc_rx = app_state.db.cdc_rx(&session.organization).await;
 
     // Check if a connection with this id already exists
     if WEBSOCKETS.read().await.contains_key(&id) {
@@ -45,18 +77,12 @@ pub async fn get_handler(
         // Set the maximum WebSocket message size
         .max_message_size(*WEBSOCKET_MAX_MESSAGE_SIZE)
         // Handle the WebSocket upgrade and process messages
-        .on_upgrade(move |socket| handle_socket(socket, session, id, cdc_rx, app_state.env_vars))
+        .on_upgrade(move |socket| handle_socket(socket, session, id, app_state.env_vars))
 }
 
-async fn handle_socket(
-    ws: WebSocket,
-    sess: Session,
-    id: Uuid,
-    cdc_rx: Receiver<cdc::Transaction>,
-    env_vars: EnvVars,
-) {
+async fn handle_socket(ws: WebSocket, sess: Session, id: Uuid, env_vars: EnvVars) {
     // Create a new connection instance
-    let rpc = Connection::new(id, sess, cdc_rx, env_vars);
+    let rpc = Connection::new(id, sess, env_vars);
     // Serve the socket connection requests
     Connection::serve(rpc, ws).await;
 }
