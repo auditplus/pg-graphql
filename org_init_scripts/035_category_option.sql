93588fa86cff4c0371ac9d7df0ecc79096d2f7e6
leefordjudes <leefordjudes@gmail.com>
2024-07-29 11:19:48 +0530

    category_name added in cat.opt

--- org_init_scripts/035_category_option.sql
+++ org_init_scripts/035_category_option.sql
@@ -1,10 +1,11 @@
 create table if not exists category_option
 (
-    id          int       not null generated always as identity primary key,
-    category_id text      not null,
-    name        text      not null,
-    active      boolean   not null default true,
-    updated_at  timestamp not null default current_timestamp,
+    id            int       not null generated always as identity primary key,
+    category_id   text      not null,
+    category_name text      not null,
+    name          text      not null,
+    active        boolean   not null default true,
+    updated_at    timestamp not null default current_timestamp,
     unique (category_id, name),
     constraint name_min_length check (char_length(trim(name)) > 0)
 );
@@ -14,3 +15,18 @@ create trigger sync_category_option_updated_at
     on category_option
     for each row
 execute procedure sync_updated_at();
+--##
+create function sync_category_option_catname_at()
+    returns trigger as
+$$
+begin
+    select x.category into new.category_name from category as x where x.id = new.category_id;
+    return new;
+end;
+$$ language plpgsql security definer;
+--##
+create trigger sync_category_option_catname_at
+    before insert or update
+    on category_option
+    for each row
+execute procedure sync_category_option_catname_at();
